94ef96bcdcb1e78851aca18c0e9737e7c1350e68
Kaikitou2013 <kaikitou-aws-cloud@ml.css.fujitsu.com>
2013-07-09 04:06:46 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mysqlapp::Application.config.secret_token = '0fed032ece3b6e56d542f50cabb8d4b0e8cad43198fcc08a6b54a96d2bccb1cda51ee0cb4fe6efa7d10859959f689b7b381025909e281bade80cbea61571b184'
